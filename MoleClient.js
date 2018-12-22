8bf87f3ee9f33c126efffbf2dfd28c34d61a5d26
Yaroslav <flashglbm@gmail.com>
2018-12-22 12:08:53 +0200

    deleting promises

--- MoleClient.js
+++ MoleClient.js
@@ -1,11 +1,12 @@
 const uuidv4 = require('uuid/v4');
 
 class MoleClient {
-    constructor({transport}) {
+    constructor({ transport, pendingRequestTimeout = 1000 }) {
         if (!transport) throw new Error('TRANSPORT_REQUIRED');
         this.transport = transport;
 
         this.pendingRequest = {};
+        this.pendingRequestTimeout = pendingRequestTimeout;
         this.initialized = false;
     }
 
@@ -21,19 +22,19 @@ class MoleClient {
         const response = JSON.parse(data);
         const isSuccessfulResponse = response.hasOwnProperty('result') || false;
         const isErrorResponse = response.hasOwnProperty('error');
-        
-        if ( !isSuccessfulResponse && !isErrorResponse ) return;
+
+        if (!isSuccessfulResponse && !isErrorResponse) return;
 
         const resolvers = this.pendingRequest[response.id];
         delete this.pendingRequest[response.id]; // TODO implement timeouts
 
         if (!resolvers) return;
-        
+
         if (isSuccessfulResponse) {
             resolvers.resolve(response.result)
         } else if (isErrorResponse) {
-            resolvers.reject( new Error(response.error.message) );
-        } 
+            resolvers.reject(new Error(response.error.message));
+        }
     }
 
     async callMethod(method, ...params) {
@@ -45,12 +46,17 @@ class MoleClient {
             params,
             id: uuidv4(),
         };
-        
+
         const data = JSON.stringify(request);
-        
+
 
         return new Promise((resolve, reject) => {
-            this.pendingRequest[request.id] = {resolve, reject};  
+            this.pendingRequest[request.id] = { resolve, reject };
+            setTimeout(() => {
+                    reject(new Error('Endpoint Disabled'));
+                    delete this.pendingRequest[request.id];
+                }, 
+                this.pendingRequestTimeout);
             return this.transport.send(data);
         });
     }
