a97e3765cf2c0ee14512dd342b8230ba74a1af2b
Roman Shevchuk <romansh@wix.com>
2020-03-30 00:49:20 +0300

    Use TS for MoleClient

--- src/types.ts
+++ src/types.ts
@@ -21,6 +21,10 @@ export interface RequestObject<Methods extends ExposedMethods, Method extends Me
     id?: string;
 }
 
+export type ResponseObject<Methods extends ExposedMethods, Method extends MethodName<Methods>> =
+    ResultResponseObject<Methods, Method>
+    | ErrorResponseObject;
+
 export type ResultResponseObject<Methods extends ExposedMethods, Method extends MethodName<Methods>> = {
     jsonrpc: '2.0';
     id: string;
@@ -28,19 +32,28 @@ export type ResultResponseObject<Methods extends ExposedMethods, Method extends 
 }
 
 export type ErrorResponseObject = {
-    jsonrpc: '2.0',
-    id: string
-    error: {
-        code: number,
-        message: string,
-        data?: any
-    }
+    jsonrpc: '2.0';
+    id: string;
+    error: MethodError;
 };
 
+export function isSuccessfulResponse(response: ResponseObject<any, any>): response is ResultResponseObject<any, any> {
+    return 'result' in response;
+}
+export function isErrorResponse(response: ResponseObject<any, any>): response is ErrorResponseObject {
+    return 'error' in response;
+}
 
 type Await<T> = T extends PromiseLike<infer U> ? U : T
 
 export type MethodName<Methods extends ExposedMethods> = keyof Methods;
 export type MethodParams<Methods extends ExposedMethods, Method extends MethodName<Methods>> = Parameters<Methods[Method]>
 export type MethodResult<Methods extends ExposedMethods, Method extends MethodName<Methods>> = Await<ReturnType<Methods[Method]>>
+export type MethodError = {
+    code: number,
+    message: string,
+    data?: any
+}
+
+
 export type Mode = 'notify' | 'callMethod';
